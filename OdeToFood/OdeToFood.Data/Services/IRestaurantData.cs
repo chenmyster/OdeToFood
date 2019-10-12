ebd8d5d59bda4ae89950af970f11b3bb57d46de1
Sitong Chen <s.chen329@gmail.com>
2019-10-12 23:45:34 +0100

    finished

--- OdeToFood/OdeToFood.Data/Services/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/Services/IRestaurantData.cs
@@ -10,5 +10,9 @@ namespace OdeToFood.Data.Services
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetAll();
+        Restaurant Get(int id);
+        void Add(Restaurant restaurant);
+        void Update(Restaurant restaurant);
+        void Delete(int id);
     }
 }
